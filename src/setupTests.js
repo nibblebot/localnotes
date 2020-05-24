502f251072a793d549ce1264a43fdda25c91ac3f
Joshua Hanson <nibblebot@gmail.com>
2020-05-23 21:34:09 -0500

    integration test: app renders

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect"
+const Dexie = require("dexie")
+Dexie.dependencies.indexedDB = require("fake-indexeddb")
+Dexie.dependencies.IDBKeyRange = require("fake-indexeddb/lib/FDBKeyRange")
